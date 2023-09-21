c73eef9efe1c03772ec665a37690be4f26afbd73
Rob Edwards <rob.j.edwards@gmail.com>
2023-09-21 14:16:21 +0100

    hack to get image working

--- cloud_build_codey/codey.py
+++ cloud_build_codey/codey.py
@@ -1,46 +1,31 @@
+import argparse
+import sys
+
 from google.cloud import aiplatform
-
-# import vertexai
-# from vertexai.language_models import CodeChatModel
-
-# vertexai.init(project="codey-coffee", location="us-central1")
-# chat_model = CodeChatModel.from_pretrained("codechat-bison@001")
-# parameters = {
-#     "candidate_count": 1,
-#     "max_output_tokens": 1024,
-#     "temperature": 0.2
-# }
-
-
 from vertexai.preview.language_models import ChatModel, InputOutputTextPair
 from vertexai.preview.language_models import CodeChatModel
 
-def science_tutoring(temperature: float = 0.2) -> None:
-    chat_model = ChatModel.from_pretrained("chat-bison@001")
+def documentation():
+    print('Here is some documentation .....')
 
-    # TODO developer - override these parameters as needed:
+def releasenotes(temperature: float = 0.5) -> object:
+
     parameters = {
         "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
-        "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
-        "top_p": 0.95,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
-        "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
+        "max_output_tokens": 1024,  # Token limit determines the maximum amount of text output.
     }
 
-    chat = chat_model.start_chat(
-        context="My name is Miles. You are an astronomer, knowledgeable about the solar system.",
-        examples=[
-            InputOutputTextPair(
-                input_text="How many moons does Mars have?",
-                output_text="The planet Mars has two moons, Phobos and Deimos.",
-            ),
-        ],
-    )
+    code_chat_model = CodeChatModel.from_pretrained("codechat-bison@001")
+    chat = code_chat_model.start_chat()
 
     response = chat.send_message(
-        "How many planets are there in the solar system?", **parameters
+        "Please help write release notes for def min(a, b): if a < b: return a else: return b", **parameters
     )
     print(f"Response from Model: {response.text}")
 
+    # [END aiplatform_sdk_code_chat]
+
+    return response
 
 
 def write_a_function(temperature: float = 0.5) -> object:
@@ -83,8 +68,28 @@ def release_notes(temperature: float = 0.5) -> object:
     return response
 
 
-if __name__ == "__main__":
-    # science_tutoring()
-    # write_a_function()
-    release_notes()
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+parser_documentation = subparsers.add_parser('documentation', help='Generate docuemntation for provided code')
+parser_documentation.set_defaults(func=documentation)
+      
+parser_release_notes = subparsers.add_parser('release-notes', help='Generate Release notes')
+parser_release_notes.set_defaults(func=releasenotes)
+
+if len(sys.argv) <= 1:
+    sys.argv.append('--help')
+
+options = parser.parse_args()
+# Run the appropriate function
+options.func()
+
+# If you add command-line options, consider passing them to the function,
+# e.g. `options.func(options)`
+
+# if __name__ == "__main__":
+#     # science_tutoring()
+#     # write_a_function()
+#     release_notes()
 
