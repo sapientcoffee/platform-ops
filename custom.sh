de06b62f72dba68e5e352d1356d144023d9384e8
Rob Edwards <rob.j.edwards@gmail.com>
2023-11-01 21:23:56 +0000

    typo fix

--- custom.sh
+++ custom.sh
@@ -1,7 +1,6 @@
-# cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/
+cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/
 
 
-# git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k && echo "installed powerlevel10k theme for zsh"
+git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k
 
-# cp /sapientcoffee/settings/zshrc ~/.zshrc
-# # COPY zshrc /home/user/.zshrc
+cp /sapientcoffee/settings/zshrc ~/.zshrc
