00247b297416f917203af0ebdfa4e90a65644811
Rob Edwards <rob.j.edwards@gmail.com>
2023-11-01 22:17:07 +0000

    default to opening zsh

--- custom.sh
+++ custom.sh
@@ -4,3 +4,4 @@ cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/M
 git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k
 
 cp /sapientcoffee/settings/zshrc ~/.zshrc
+echo "exec zsh" >> ~/.bashrc
