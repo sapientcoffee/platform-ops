5150719f06d568def5bfb297d1b43badc24f8544
Rob Edwards <rob.j.edwards@gmail.com>
2023-11-01 20:58:59 +0000

    empty script to see if that the issue

---
+++ custom.sh
@@ -0,0 +1,7 @@
+# cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/
+
+
+# git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k && echo "installed powerlevel10k theme for zsh"
+
+# cp /sapientcoffee/settings/zshrc ~/.zshrc
+# # COPY zshrc /home/user/.zshrc
