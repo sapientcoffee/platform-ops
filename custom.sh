6f9b701ba15d9f215f83fdbdf5d50d1b0afca86d
Rob Edwards <rob.j.edwards@gmail.com>
2023-11-08 15:42:53 +0000

    pilot different way to setup custominsation

--- custom.sh
+++ custom.sh
@@ -1,7 +1,10 @@
-cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/
+# cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/
 
 
-git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k
+# git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k
 
-cp /sapientcoffee/settings/zshrc ~/.zshrc
-echo "exec zsh" >> ~/.bashrc
+# cp /sapientcoffee/settings/zshrc ~/.zshrc
+# echo "exec zsh" >> ~/.bashrc
+
+echo "hello world"
+
