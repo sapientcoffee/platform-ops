abd3b77a6b996b696c4f0b0a756d5b6d6c2b60b2
Rob Edwards <rob.j.edwards@gmail.com>
2023-11-08 23:12:23 +0000

    .

--- custom.sh
+++ custom.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 runuser user -c "touch ~/.rob"
-# runuser user -c -l "cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/"
+runuser user -c "cp /sapientcoffee/settings/settings.json /home/user/.codeoss-cloudworkstations/data/Machine/"
 
 
-# runuser user -c -l "git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k"
+runuser user -c "git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k"
 
-# runuser user -c -l "cp /sapientcoffee/settings/zshrc ~/.zshrc"
-# runuser user -c -l "echo "exec zsh" >> ~/.bashrc"
+runuser user -c "cp /sapientcoffee/settings/zshrc ~/.zshrc"
+runuser user -c "cp /sapientcoffee/settings/zshrc ~/.zshrc-cp"
+runuser user -c "echo "exec zsh" >> ~/.bashrc"
 
 # echo "hello world"
 
