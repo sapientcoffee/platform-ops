a63905ed94ab285e717e5f815c6c0ac31a1d572b
Rob Edwards <rob.j.edwards@gmail.com>
2023-11-08 17:53:33 +0000

    testing runuser for envvar

--- custom.sh
+++ custom.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 runuser user -c "export TEST_VAR=rob"
-runuser user -c -l "cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/"
+# runuser user -c -l "cp /sapientcoffee/settings/settings.json $HOME/.codeoss-cloudworkstations/data/Machine/"
 
 
-runuser user -c -l "git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k"
+# runuser user -c -l "git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k"
 
-runuser user -c -l "cp /sapientcoffee/settings/zshrc ~/.zshrc"
-runuser user -c -l "echo "exec zsh" >> ~/.bashrc"
+# runuser user -c -l "cp /sapientcoffee/settings/zshrc ~/.zshrc"
+# runuser user -c -l "echo "exec zsh" >> ~/.bashrc"
 
 # echo "hello world"
 
