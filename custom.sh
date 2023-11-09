d488fe81f4cc270fa6a2f769be349348a6d9536c
Rob Edwards <rob.j.edwards@gmail.com>
2023-11-09 16:12:33 +0000

    refactor customisation

--- custom.sh
+++ custom.sh
@@ -1,11 +1,25 @@
-#!/bin/sh
-runuser user -c "touch ~/.rob"
-runuser user -c "cp /sapientcoffee/settings/settings.json /home/user/.codeoss-cloudworkstations/data/Machine/"
-runuser user -c "cp /sapientcoffee/settings/.p10k.zsh /home/user/"
+#!/usr/bin/env bash
 
-runuser user -c "sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && echo "installed oh-my-zsh""
-runuser user -c "git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k"
+set -e # bail out early if any command fails
+set -u # fail if we hit unset variables
+set -o pipefail # fail if any component of any pipe fails
 
-runuser user -c "cp /sapientcoffee/settings/zshrc ~/.zshrc"
-runuser user -c "cp /sapientcoffee/settings/zshrc ~/.zshrc-cp"
-runuser user -c "echo "exec zsh" >> /home/user/.bashrc"
+runuser user -c 'touch ~/.rob'
+
+echo 'cleaning up previous installs'
+runuser user -c 'rm -rf /home/user/.oh-my-zsh/'
+
+echo 'install oh-my-zsh'
+runuser user -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+echo 'install powerlevel10 theme'
+runuser user -c 'git clone https://github.com/romkatv/powerlevel10k.git /home/user/.oh-my-zsh/custom/themes/powerlevel10k'
+
+echo 'Copy IDE settings and terminal settings'
+runuser user -c 'cp /sapientcoffee/settings/settings.json /home/user/.codeoss-cloudworkstations/data/Machine/'
+runuser user -c 'cp /sapientcoffee/settings/.p10k.zsh /home/user/'
+runuser user -c 'cp /sapientcoffee/settings/zshrc /home/user/.zshrc'
+runuser user -c 'cp /sapientcoffee/settings/.p10k.zsh /home/user/'
+
+echo 'set zsh as default'
+runuser user -c 'echo 'exec zsh' >> /home/user/.bashrc'
+
